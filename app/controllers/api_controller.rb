9df9662f8968c231a3110a35d639d6cc536d6a11
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2024-04-06 08:13:04 +0900

    api更新

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -174,7 +174,7 @@ class ApiController < ApplicationController
     season = params[:season].blank? ? Season.find_by(finished_at: nil) : Season.find_by(id: params[:season])
     data[:season] = season.name
     # 壁なしシングルTOP5取得
-    nw_s = season.users.select(:name, :nw_s_rate, :nw_s_win, :nw_s_lose, :id).where.not(nw_s_win: 0, nw_s_lose: 0).order(nw_s_rate: :desc, nw_s_win: :desc, updated_at: :asc).limit(5)
+    nw_s = season.users.select("name, nw_s_rate as nowall_single_rate, nw_s_win as nowall_single_win, nw_s_lose as nowall_single_lose, id").where.not(nw_s_win: 0, nw_s_lose: 0).order(nw_s_rate: :desc, nw_s_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if nw_s.size < i
         break;
@@ -186,7 +186,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁なしダブル取得
-    nw_d = season.users.select(:name, :nw_d_rate, :nw_d_win, :nw_d_lose, :id).where.not(nw_d_win: 0, nw_d_lose: 0).order(nw_d_rate: :desc, nw_d_win: :desc, updated_at: :asc).limit(5)
+    nw_d = season.users.select("name, nw_d_rate as nowall_double_rate, nw_d_win as nowall_double_win, nw_d_lose as nowall_double_lose, id").where.not(nw_d_win: 0, nw_d_lose: 0).order(nw_d_rate: :desc, nw_d_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if nw_d.size < i
         break;
@@ -198,7 +198,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁ありシングル取得
-    ow_s = season.users.select(:name, :ow_s_rate, :ow_s_win, :ow_s_lose, :id).where.not(ow_s_win: 0, ow_s_lose: 0).order(ow_s_rate: :desc, ow_s_win: :desc, updated_at: :asc).limit(5)
+    ow_s = season.users.select("name, ow_s_rate as onwall_single_rate, ow_s_win as onwall_single_win, ow_s_lose as onwall_single_lose, id").where.not(ow_s_win: 0, ow_s_lose: 0).order(ow_s_rate: :desc, ow_s_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if ow_s.size < i
         break;
@@ -210,7 +210,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁ありダブル取得
-    ow_d = season.users.select(:name, :ow_d_rate, :ow_d_win, :ow_d_lose, :id).where.not(ow_d_win: 0, ow_d_lose: 0).order(ow_d_rate: :desc, ow_d_win: :desc, updated_at: :asc).limit(5)
+    ow_d = season.users.select("name, ow_d_rate as onwall_double_rate, ow_d_win as onwall_double_win, ow_d_lose as onwall_double_lose, id").where.not(ow_d_win: 0, ow_d_lose: 0).order(ow_d_rate: :desc, ow_d_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if ow_d.size < i
         break;
@@ -225,8 +225,9 @@ class ApiController < ApplicationController
   end
 
   def getrank
+    data = {}
     season = params[:season].blank? ? Season.find_by(finished_at: nil) : Season.find_by(id: params[:season])
-    data = season.users.select(:name, :nw_s_rate, :nw_s_win, :nw_s_lose).where.not(nw_s_win: 0, nw_s_lose: 0, nw_d_win: 0, nw_d_lose: 0 ,ow_s_win: 0, ow_s_lose: 0, ow_d_win: 0, ow_d_lose: 0)
+    data[:rank] = season.users.select("name, nw_s_rate as nowall_single_rate, nw_s_win as nowall_single_win, nw_s_lose as nowall_single_lose, nw_d_rate as nowall_double_rate, nw_d_win as nowall_double_win, nw_d_lose as nowall_double_lose, ow_s_rate as onwall_single_rate, ow_s_win as onwall_single_win, ow_s_lose as onwall_single_lose, ow_d_rate as onwall_double_rate, ow_d_win as onwall_double_win, ow_d_lose as onwall_double_lose, id").where.not(nw_s_win: 0, nw_s_lose: 0, nw_d_win: 0, nw_d_lose: 0 ,ow_s_win: 0, ow_s_lose: 0, ow_d_win: 0, ow_d_lose: 0)
     data[:api_ver] = Settings.api_ver
     render json: data.to_json
   end
@@ -249,7 +250,7 @@ class ApiController < ApplicationController
     season = params[:season].blank? ? Season.find_by(finished_at: nil) : Season.find_by(id: params[:season])
     data[:season] = season.name
     # 壁なしシングルTOP5取得
-    nw_s = season.users.select(:name, :nw_s_rate, :nw_s_win, :nw_s_lose, :id).where.not(nw_s_win: 0, nw_s_lose: 0).order(nw_s_rate: :desc, nw_s_win: :desc, updated_at: :asc).limit(5)
+    nw_s = season.users.select("name, nw_s_rate as nowall_single_rate, nw_s_win as nowall_single_win, nw_s_lose as nowall_single_lose, id").where.not(nw_s_win: 0, nw_s_lose: 0).order(nw_s_rate: :desc, nw_s_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if nw_s.size < i
         break;
@@ -261,7 +262,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁なしダブル取得
-    nw_d = season.users.select(:name, :nw_d_rate, :nw_d_win, :nw_d_lose, :id).where.not(nw_d_win: 0, nw_d_lose: 0).order(nw_d_rate: :desc, nw_d_win: :desc, updated_at: :asc).limit(5)
+    nw_d = season.users.select("name, nw_d_rate as nowall_double_rate, nw_d_win as nowall_double_win, nw_d_lose as nowall_double_lose, id").where.not(nw_d_win: 0, nw_d_lose: 0).order(nw_d_rate: :desc, nw_d_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if nw_d.size < i
         break;
@@ -277,7 +278,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁ありシングル取得
-    ow_s = season.users.select(:name, :ow_s_rate, :ow_s_win, :ow_s_lose, :id).where.not(ow_s_win: 0, ow_s_lose: 0).order(ow_s_rate: :desc, ow_s_win: :desc, updated_at: :asc).limit(5)
+    ow_s = season.users.select("name, ow_s_rate as onwall_single_rate, ow_s_win as onwall_single_win, ow_s_lose as onwall_single_lose, id").where.not(ow_s_win: 0, ow_s_lose: 0).order(ow_s_rate: :desc, ow_s_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if ow_s.size < i
         break;
@@ -293,7 +294,7 @@ class ApiController < ApplicationController
       end
     end
     # 壁ありダブル取得
-    ow_d = season.users.select(:name, :ow_d_rate, :ow_d_win, :ow_d_lose, :id).where.not(ow_d_win: 0, ow_d_lose: 0).order(ow_d_rate: :desc, ow_d_win: :desc, updated_at: :asc).limit(5)
+    ow_d = season.users.select("name, ow_d_rate as onwall_double_rate, ow_d_win as onwall_double_win, ow_d_lose as onwall_double_lose, id").where.not(ow_d_win: 0, ow_d_lose: 0).order(ow_d_rate: :desc, ow_d_win: :desc, updated_at: :asc).limit(5)
     for i in 1..5
       if ow_d.size < i
         break;
@@ -308,7 +309,7 @@ class ApiController < ApplicationController
         data[:top][:onwall_double][ApiHelper::PLACE[i]] = ow_d[i - 1]
       end
     end
-    data[:rank] = season.users.select(:name, :nw_s_rate, :nw_s_win, :nw_s_lose).where.not(nw_s_win: 0, nw_s_lose: 0, nw_d_win: 0, nw_d_lose: 0 ,ow_s_win: 0, ow_s_lose: 0, ow_d_win: 0, ow_d_lose: 0)
+    data[:rank] = season.users.select("name, nw_s_rate as nowall_single_rate, nw_s_win as nowall_single_win, nw_s_lose as nowall_single_lose, nw_d_rate as nowall_double_rate, nw_d_win as nowall_double_win, nw_d_lose as nowall_double_lose, ow_s_rate as onwall_single_rate, ow_s_win as onwall_single_win, ow_s_lose as onwall_single_lose, ow_d_rate as onwall_double_rate, ow_d_win as onwall_double_win, ow_d_lose as onwall_double_lose, id").where.not(nw_s_win: 0, nw_s_lose: 0, nw_d_win: 0, nw_d_lose: 0 ,ow_s_win: 0, ow_s_lose: 0, ow_d_win: 0, ow_d_lose: 0)
     data[:status] = {:season => {:name => season.name, :id => season.id, :started_at => season.created_at, :finished_at => season.finished_at}}
     data[:status][:players] = {:nowall_single => season.users.where.not(nw_s_win: 0, nw_s_lose: 0).count(),
                                :nowall_double => season.users.where.not(nw_d_win: 0, nw_d_lose: 0).count(),
